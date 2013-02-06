4c9dc16d3172b50ffc6c91850604fc45123c95de
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2013-02-05 22:41:09 -0200

    Estudos de roles e heranca de roles

---
+++ zend_acl.php
@@ -0,0 +1,21 @@
+<?php
+require 'Zend/Loader.php';
+Zend_Loader::registerAutoload();
+
+echo Zend_Version ::VERSION;
+
+$acl = new Zend_Acl();
+
+# Roles
+$admin = new Zend_Acl_Role('Admin');
+$oreiaSeca = new Zend_Acl_Role('Oreia');
+$manageador = new Zend_Acl_Role('Manager');
+
+$acl->addRole($oreiaSeca);
+$acl->addRole($manageador);
+
+$acl->addRole($admin, array('Oreia', $manageador));
+
+Zend_Debug::dump( $acl );
+
+# Resources
